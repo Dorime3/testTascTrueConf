44af6c3adcdb71501dff40326c2fad052c3855fd
Rodion <redia1994@mail.ru>
2021-10-26 00:35:27 +0300

    settimeoutforcolors

--- src/App.js
+++ src/App.js
@@ -4,31 +4,10 @@ import { Timer } from './components/Timer';
 import { TrafficLights } from './components/TrafficLights';
 
 function App() {
-  const [color, setColor] = useState('red')
-  const timeoutColor = [
-    {
-      color: 'red',
-      time: 15000
-    },
-    {
-      color: 'yellow',
-      time: 3000
-    },
-    {
-      color: 'green',
-      time: 10000
-    }]
-  useEffect(() => {
-    setInterval(() => {
-      setColor('yellow')
-    }, 10000)
-    return () => {
-      clearInterval()
-    }
-  }, [])
+
   return (
     <div className="App">
-      <TrafficLights color={color} />
+      <TrafficLights />
       <Timer />
     </div>
   );
--- src/components/TrafficLights.jsx
+++ src/components/TrafficLights.jsx
@@ -6,12 +6,36 @@ import s from './TrafficLights.module.scss';
 import { Yellow } from './Yellow';
 
 export const TrafficLights = (props) => {
-
-
+    const [colorNum, setColorNum] = useState(0)
+    const [color, setColor] = useState('red')
+    const timeoutColor = [
+        {
+            color: 'red',
+            time: 3000
+        },
+        {
+            color: 'yellow',
+            time: 10000
+        },
+        {
+            color: 'green',
+            time: 3000
+        },
+        {
+            color: 'yellow',
+            time: 15000
+        }]
+    useEffect(() => {
+        const { color, time } = timeoutColor[colorNum];
+        const timer = setTimeout(() => {
+            setColorNum((colorNum + 1) % 4);
+            setColor(() => color);
+        }, time)
+    }, [colorNum])
     return (
         <div className={s.wrapperTL}>
             <div className={s.trafficLights}>
-                {props.color && <Redirect to={`/${props.color}`} />}
+                {color && <Redirect to={`/${color}`} />}
                 <div className={s.trafficColor + ' ' + s.offRed}>
                     <Route path='/red' component={Red} />
                 </div>
